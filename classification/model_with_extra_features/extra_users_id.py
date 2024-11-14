0739f3e4445b4ab62b515c4ef2aff9eee82a1701
GrinnerH <wangwenhao.work@gmail.com>
2024-11-14 19:42:44 +0800

    修改自己的脚本

--- classification/model_with_extra_features/extra_users_id.py
+++ classification/model_with_extra_features/extra_users_id.py
@@ -2,12 +2,15 @@ import pandas as pd
 
 # 使用绝对路径读取 Parquet 文件
 df = pd.read_parquet("/home/robot/wwh/ViralTweets/classification/model_with_extra_features/final_dataset_since_october_2022.parquet.gzip")
-df.to_csv("all_tweets.csv")
+#df.to_csv("all_tweets.csv")
 # 提取 author_id 列并去重
-author_ids = df['author_id'].drop_duplicates()
-
-# 打印结果
-print(author_ids)
-
+# author_ids = df['author_id'].drop_duplicates()
 # 保存到 CSV
-author_ids.to_csv("users_ids.csv", index=False)
+# author_ids.to_csv("users_ids.csv", index=False)
+# 将 Series 转换为 DataFrame
+# author_ids_df = author_ids.to_frame(name='author_id')
+
+# 保存到 Parquet 文件
+# author_ids_df.to_parquet('users.parquet.gzip', compression='gzip')
+
+df.to_excel('all_tweets_with_features.xlsx')
